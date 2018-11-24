45d13ea2b716c2757f645eebb4c52aa4979b806a
graceharper <gharper31@gatech.edu>
2018-11-24 16:24:10 -0500

    cleaned up

--- s1.py
+++
@@ -1,5 +0,0 @@
-import star_node as sn
-
-s1 = sn.StarNode("A", 8888, "localhost", 8000, 2)
-
-print("so intelligent")
--- s2.py
+++
@@ -1,3 +0,0 @@
-import star_node as sn
-s2 = sn.StarNode("B", 8000, "localhost", 8888, 2)
-
